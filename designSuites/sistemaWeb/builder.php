7c937668479739217bd683fc367893de2b7913db
rodrigoacosta46 <roroacostacant@gmail.com>
2023-08-11 16:55:59 -0300

    Estructura beta del programa

---
+++ designSuites/sistemaWeb/builder.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Nav bar -->
+
+    <!-- Reservas -->
+
+    <div id="contenedorPrincipal">
+        <?php //require_once 'views/' . $_GET['sec']?>
+    </div>
+</body>
+</html>
