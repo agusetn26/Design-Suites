46549ff0ba81a5d3172894fe9c9c59b806302eb6
agusetn26 <augusto.etn26@gmail.com>
2023-08-25 16:45:10 -0300

    termino pagPrincipal

--- sistemaWeb/views/home.php
+++ sistemaWeb/views/home.php
@@ -5,36 +5,47 @@
         <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class="active" aria-current="true"></button>
         <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
     </div>
     <div class="carousel-inner">
-        <div class="carousel-item">
-            <img src="../img/goku-ssj-3.jpg" style="width: 100%; height: auto;" alt="">
-            <div class="container">
-                <div class="carousel-caption text-start">
-                    <h1>Example headline.</h1>
-                    <p class="opacity-75">Some representative placeholder content for the first slide of the carousel.</p>
-                    <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
-                </div>
-            </div>
-        </div>
         <div class="carousel-item active">
-            <img src="../img/fondoxd.jpeg" style="width: 100%; height: auto;" alt="">
-            <div class="container">
-                <div class="carousel-caption">
-                    <h1>Another example headline.</h1>
-                    <p>Some representative placeholder content for the second slide of the carousel.</p>
-                    <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
-                </div>
+            <div class="content"> <a href="#">
+                    <div class="content-overlay"></div> <img class="content-image img-carrusel" src="../img/salta.jpg" style="width: 1903; height: auto;" alt="">
+                    <div class="content-details fadeIn-bottom">
+                        <h3 class="content-title" style="font-size: 50px;">Salta</h3>
+                        <p class="content-text" style="font-size: 35px;"><i class="fa fa-map-marker"></i> Design Suites Hoteles</p>
+                    </div>
+                </a>
             </div>
         </div>
         <div class="carousel-item">
-            <img src="../img/goku-ssj-3.jpg" style="width: 100%; height: auto;" alt="">
-            <div class="container">
-                <div class="carousel-caption text-end">
-                    <h1>One more for good measure.</h1>
-                    <p>Some representative placeholder content for the third slide of this carousel.</p>
-                    <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
-                </div>
+            <div class="content"> <a href="#">
+                    <div class="content-overlay"></div> <img class="content-image img-carrusel" src="../img/bariloche.jpg" style="width: 1903; height: auto;" alt="">
+                    <div class="content-details fadeIn-bottom">
+                        <h3 class="content-title" style="font-size: 50px;">Bariloche</h3>
+                        <p class="content-text" style="font-size: 35px;"><i class="fa fa-map-marker"></i> Design Suites Hoteles</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="content"> <a href="#">
+                    <div class="content-overlay"></div> <img class="content-image img-carrusel" src="../img/calafate.jpg" style="width: 1903; height: auto;" alt="">
+                    <div class="content-details fadeIn-bottom">
+                        <h3 class="content-title" style="font-size: 50px;">Calafate</h3>
+                        <p class="content-text" style="font-size: 35px;"><i class="fa fa-map-marker"></i> Design Suites Hoteles</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="content"> <a href="#">
+                    <div class="content-overlay"></div> <img class="content-image img-carrusel" src="../img/BsAs.png" style="width: 1903; height: auto;" alt="">
+                    <div class="content-details fadeIn-bottom">
+                        <h3 class="content-title" style="font-size: 50px;">Buenos Aires</h3>
+                        <p class="content-text" style="font-size: 35px;"><i class="fa fa-map-marker"></i>Design Suites Hoteles </p>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
@@ -47,3 +58,84 @@
         <span class="visually-hidden">Next</span>
     </button>
 </div>
+
+<!--------Hoteles------------>
+
+
+<div class="text-center">
+    <p class="my-2" style="font-size: 45px;"> Nuestos Hoteles </p>
+</div>
+<!--
+<div>
+
+    <div class="hoteles">
+
+        <div class="hotel">
+            <div class="carousel-none">
+                <a href="#">
+                    <img src="../img/home_bariloche.jpg" alt="Design Suites Bariloche">
+                </a>
+                <div class="single-blog-text">
+                    <div class="blog-post-info bg-violet pl-20 pr-20 pt-17 pb-17">
+                        <span><i class="fa fa-location-arrow mr-12"></i>BARILOCHE</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</div>-->
+
+<div class="album py-5 bg-body-tertiary">
+    <div class="container">
+
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 g-5">
+            <div class="col hentai">
+                <div class="card shadow-sm">
+                    <a href="#">
+                        <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="../img/home_bariloche.jpg" alt="Design Suites Bariloche">
+                    </a>
+                    <div class="card-body py-3 px-5" style="background-color: #20233f; ">
+                        <p class="card-text" style="color: white; font-size:20px; font-weight:bold;">BARILOCHE</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col hentai">
+                <div class="card shadow-sm">
+                    <a href="#">
+                        <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="../img/home_buenos_aires.jpg" alt="Design Suites Bariloche">
+                    </a>
+                    <div class="card-body py-3 px-5" style="background-color: #20233f; ">
+                        <p class="card-text" style="color: white; font-size:20px; font-weight:bold;">BUENSO AIRES</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col hentai">
+                <div class="card shadow-sm">
+                    <a href="#">
+                        <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="../img/home_calafate.jpg" alt="Design Suites Bariloche">
+                    </a>
+                    <div class="card-body py-3 px-5" style="background-color: #20233f; ">
+                        <p class="card-text" style="color: white; font-size:20px; font-weight:bold;">CALAFATE</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col hentai">
+                <div class="card shadow-sm">
+                    <a href="#">
+                        <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="../img/home_salta.jpg" alt="Design Suites Bariloche">
+                    </a>
+                    <div class="card-body py-3 px-5" style="background-color: #20233f; ">
+                        <p class="card-text" style="color: white; font-size:20px; font-weight:bold;">SALTA</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<br>
