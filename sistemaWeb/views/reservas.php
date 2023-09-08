877bd185438c2e8e3e7a4bc47a3b8da28b833d47
agusetn26 <augusto.etn26@gmail.com>
2023-09-08 15:39:49 -0300

    yo-soy-el-cum-master

---
+++ sistemaWeb/views/reservas.php
@@ -0,0 +1,76 @@
+<div class="container my-5">
+    <div class="row">
+
+        <!-- izq -->
+        <div class="col-sm my-1">
+            <div class="h4 col-sm bg-dark p-3 text-light rounded">
+                Buscar
+            </div>
+            <div class="col bg-gris text-light px-4 py-3 rounded    ">
+                <div class="row mb-3">
+                    Desde
+                    <input class="w-100 p-1" type="date">
+                </div>
+                <div class="row">
+
+                    Hasta
+                    <input class="w-100 p-1" type="date">
+
+                    <button type="button" class="btn btn-outline-light my-3">Buscar</button>
+                    <a class="text-light" href="#">» Modificar / Cancelar una reserva</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- cen -->
+
+        <div class="col-sm-7 my-1">
+            <div class="col-sm bg-dark h4 p-3 text-light rounded">
+                Reservas
+            </div>
+            <div class="col bg-gris text-light p-3 mb-2 rounded">
+                <div class="row">
+                    <div class="col-5">
+                        <img src="../img/goku-ssj-3.jpg" class="img-fluid w-100 rounded imgReserva" alt="Responsive image">
+                    </div>
+                    <div class="col contenedor">
+                        <div>
+                            <p>Classic Room </p>
+                            <div><span class="material-symbols-outlined cant-per">person</span><span class="material-symbols-outlined cant-per2">person</span></div>
+                        </div>
+                        <div class="linea-in"></div>
+                        <div class="my-2 ">
+                            <span class="material-symbols-outlined icono-personalizado">contact_phone</span>
+                            <span class="material-symbols-outlined icono-personalizado">tv</span>
+                            <span class="material-symbols-outlined icono-personalizado">wifi</span>
+                            <span class="material-symbols-outlined icono-personalizado">ac_unit</span>
+                            <span class="material-symbols-outlined icono-personalizado my-2">lock</span>
+                            <span class="material-symbols-outlined icono-personalizado my-2">hot_tub</span>
+                            <span class="material-symbols-outlined icono-personalizado my-2">local_bar</span>
+                            <span class="material-symbols-outlined icono-personalizado my-2">dry</span>
+
+                        </div>
+                    </div>
+                    <div class="col">
+                        Classic Room
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="col bg-gris text-light p-3 mb-2 rounded">
+                <img src="../img/goku-ssj-3.jpg" class="img-fluid w-40 rounded imgReserva" alt="Responsive image">
+            </div>
+
+        </div>
+
+        <!-- der -->
+
+        <div class="col-sm my-1">
+            <div class="col-sm bg-dark h4 p-3 text-light rounded">
+                Buscar
+            </div>
+        </div>
+    </div>
+</div>
