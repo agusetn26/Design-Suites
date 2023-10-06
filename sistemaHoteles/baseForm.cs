881913e3ae4e12c684013700b6c44bbf9954c3d4
Angeldavidbritez <angelbritez.m26@gmail.com>
2023-10-06 11:55:00 -0300

    avances en interfaz grafica

---
+++ sistemaHoteles/baseForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sistemaHoteles
+{
+    public partial class baseForm : Form
+    {
+        public Form currentForm;
+        public baseForm()
+        {
+            InitializeComponent();
+        }
+
+        public void openForm(Form form)
+        {
+            basePanel.Controls.Clear();
+
+            currentForm = form;
+            currentForm.TopLevel = false;
+            currentForm.FormBorderStyle = FormBorderStyle.None;
+            currentForm.Dock = DockStyle.Fill;
+            basePanel.Controls.Add(currentForm);
+            currentForm.Show();
+        }
+
+        private void btnHoteles_Click(object sender, EventArgs e)
+        {
+            openForm(new hoteles());
+        }
+
+        private void reservas_Click(object sender, EventArgs e)
+        {
+            openForm(new reservas());
+        }
+    }
+}
---
+++ sistemaHoteles/hoteles.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace sistemaHoteles
+{
+    partial class hoteles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(hoteles));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.selectFile = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.select1 = new System.Windows.Forms.Button();
+            this.select2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 77.27273F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090909F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.63636F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(320, 450);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(30, 30);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(30, 40, 30, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 271);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // selectFile
+            // 
+            this.selectFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.selectFile.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.selectFile.Location = new System.Drawing.Point(64, 0);
+            this.selectFile.Margin = new System.Windows.Forms.Padding(0);
+            this.selectFile.Name = "selectFile";
+            this.selectFile.Size = new System.Drawing.Size(192, 47);
+            this.selectFile.TabIndex = 0;
+            this.selectFile.Text = "Seleccionar";
+            this.selectFile.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 3;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.Controls.Add(this.select2, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.selectFile, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.select1, 2, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 331);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(320, 47);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // select1
+            // 
+            this.select1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("select1.BackgroundImage")));
+            this.select1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.select1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.select1.FlatAppearance.BorderSize = 0;
+            this.select1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.select1.Location = new System.Drawing.Point(259, 3);
+            this.select1.Name = "select1";
+            this.select1.Size = new System.Drawing.Size(58, 41);
+            this.select1.TabIndex = 0;
+            this.select1.UseVisualStyleBackColor = true;
+            // 
+            // select2
+            // 
+            this.select2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("select2.BackgroundImage")));
+            this.select2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.select2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.select2.FlatAppearance.BorderSize = 0;
+            this.select2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.select2.Location = new System.Drawing.Point(3, 3);
+            this.select2.Name = "select2";
+            this.select2.Size = new System.Drawing.Size(58, 41);
+            this.select2.TabIndex = 1;
+            this.select2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(330, 10);
+            this.panel1.Margin = new System.Windows.Forms.Padding(10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(460, 430);
+            this.panel1.TabIndex = 1;
+            // 
+            // hoteles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "hoteles";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button selectFile;
+        private System.Windows.Forms.Button select1;
+        private System.Windows.Forms.Button select2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
